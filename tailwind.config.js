d1c1e562d0acbd3a426f270d13654e46eaa9d6e8
Roberto Tauille <robertotauille@gmail.com>
2022-12-17 13:15:06 -0300

    fix: improves tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./stories/*"],
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./stories/*"],
   theme: {
     extend: {},
   },
